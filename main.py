aee5a8d60f78bb46c2cd3dda329bf3af4b395ed7
Vitaly <vitlanglemez@gmail.com>
2025-05-23 12:44:16 +0200

    v.0.1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 
 bank = pandas.read_csv("bank.csv", sep=";")
@@ -11,14 +11,11 @@ bank = pandas.get_dummies(bank, columns=["job", "marital", "poutcome", "housing"
 bank["y"] = bank["y"].map({"no": 0, "yes": 1}).astype(int)
             
 t_x = bank.drop(columns=["y"])
-t_y = bank.loc[:, ["y"]]
+t_y = bank["y"]
 
 x_train, x_test, y_train, y_test = train_test_split(t_x, t_y, test_size=0.2, random_state=50)
 
-y_train = y_train.values.ravel()
-y_test = y_test.values.ravel()
-
-modele = LogisticRegression(max_iter=1000)
+modele = KNeighborsClassifier(n_neighbors=5)
 modele.fit(x_train, y_train)
 
 y_pred = modele.predict(x_test)

b0590e3f68be0d97d742970105480c1886f647cd
Vitaly <vitlanglemez@gmail.com>
2025-05-23 14:59:45 +0200

    v.1

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 
-bank = pandas.read_csv("bank.csv", sep=";")
-bank = bank.drop(columns=["age", "education", "default", "contact", "day", "month", "duration", "pdays", "previous"])
+bank = pandas.read_csv("bank-full.csv", sep=";")
+bank = bank.drop(columns=["age", "education", "default", "contact", "day", "month", "campaign"])
 
-bank = pandas.get_dummies(bank, columns=["job", "marital", "poutcome", "housing", "loan"], dtype=int)
+bank = pandas.get_dummies(bank, columns=["job", "marital", "housing", "loan", "poutcome", "previous"], dtype=int)
 
 bank["y"] = bank["y"].map({"no": 0, "yes": 1}).astype(int)
             
@@ -22,3 +22,55 @@ y_pred = modele.predict(x_test)
 
 score = metrics.accuracy_score(y_test, y_pred)
 print("Précision du modèle :", score*100, "%")
+
+def predict_client(client_dict):
+    """
+    Prédit si un client X aurait souscrit un compte de dépôts
+
+    :param client_dict: dict, les données du client X
+    :return: str, le résultat de la prédiction
+    """
+    client = pandas.DataFrame([client_dict])
+    
+    client = pandas.get_dummies(client, columns=["job", "marital", "housing", "loan", "poutcome", "previous"], dtype=int)
+    
+    #ajoute les colonnes manquantes pour que client_df ait les mêmes colonnes que les données d'entraînement
+    for colonne in t_x.columns:
+        if colonne not in client.columns:
+            client[colonne] = 0
+    client = client[t_x.columns]
+    
+    prediction = modele.predict(client)
+    
+    if prediction[0] == 1:
+        return "oui"
+    else:
+        return "non"
+
+#TESTS
+client_X = {
+    "job": "unemployed",
+    "marital": "married",
+    "balance": 1787,
+    "housing": "no",
+    "loan": "no",
+    "duration": 79,
+    "pdays": -1,
+    "previous": 0,
+    "poutcome": "unknown",
+}
+
+client_Y = {
+    "job": "management",
+    "marital": "single",
+    "balance": 2536,
+    "housing": "yes",
+    "loan": "no",
+    "duration": 958,
+    "pdays": -1,
+    "previous": 0,
+    "poutcome": "unknown",
+}
+
+print("Prédiction pour le client X :", predict_client(client_X))
+print("Prédiction pour le client Y :", predict_client(client_Y))

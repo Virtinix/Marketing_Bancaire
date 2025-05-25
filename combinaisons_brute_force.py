682eff774edd3b20da7b0a309eace03e3b2a08ea
Vitaly <vitlanglemez@gmail.com>
2025-05-25 19:11:43 +0200

    v1.2

---
+++ combinaisons_brute_force.py
@@ -0,0 +1,58 @@
+import pandas
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+from itertools import combinations
+
+liste_descripteurs = ["age", "job", "marital", "education", "default", "balance", "housing", "loan",
+              "contact", "day", "month", "duration", "campaign", "pdays", "previous", "poutcome"]
+
+non_numerical_descripteurs = ["job", "marital", "education", "default", "housing", "loan", "contact", "month", "poutcome"]
+
+bank = pandas.read_csv("bank-full.csv", sep=";").sample(n=2000, random_state=50)
+bank["y"] = bank["y"].map({"no": 0, "yes": 1}).astype(int)
+bank_encoded = pandas.get_dummies(bank, columns=non_numerical_descripteurs, dtype=int)
+
+def test_combinaison(combinaison):
+    """
+    Teste une combinaison sur un échantillon de 2000 valeurs prises dans bank-full.csv
+
+    :param combinaison: list, la combinaison testée
+    :return: float, le score de la combinaison
+    """
+    colonnes = []
+    
+    for descripteur in combinaison:
+        for col in bank_encoded.columns:
+            if descripteur in col and col != "y":
+                colonnes.append(col)
+                
+    t_x = bank_encoded[colonnes]
+    t_y = bank_encoded["y"]
+    x_train, x_test, y_train, y_test = train_test_split(t_x, t_y, test_size=0.2, random_state=50)
+    
+    modele = KNeighborsClassifier(n_neighbors=5)
+    modele.fit(x_train, y_train)
+    
+    y_pred = modele.predict(x_test)
+    score = metrics.accuracy_score(y_test, y_pred)
+    return score
+
+def brute_force():
+    """
+    Teste toutes les combinaisons possible (jusque 6 de longueur max) et affiche la meilleure d'entre elles et son score
+    
+    :return: affichage
+    """
+    best_score = 0
+    best_combinaison = None
+    for i in range(1, 6): #limite la taille des combinaisons à 6 max (sinon trop long)
+        for combinaison in combinations(liste_descripteurs, i):
+            score = test_combinaison(combinaison)
+            if score > best_score:
+                best_score = score
+                best_combinaison = combinaison
+    print("Meilleure combinaison :", best_combinaison)
+    print("Score :", round(best_score*100, 2), "%")
+
+brute_force() #temps de calcul ~2min
--- main.py
+++ main.py
@@ -3,25 +3,43 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 
-bank = pandas.read_csv("bank-full.csv", sep=";")
-bank = bank.drop(columns=["age", "education", "default", "contact", "day", "month", "campaign"])
+liste_descripteurs = ["age", "job", "marital", "education", "default", "balance", "housing", "loan",
+              "contact", "day", "month", "duration", "campaign", "pdays", "previous", "poutcome"]
 
-bank = pandas.get_dummies(bank, columns=["job", "marital", "housing", "loan", "poutcome", "previous"], dtype=int)
+non_numerical_descripteurs = ["job", "marital", "education", "default", "housing", "loan", "contact", "month", "poutcome"]
 
-bank["y"] = bank["y"].map({"no": 0, "yes": 1}).astype(int)
-            
-t_x = bank.drop(columns=["y"])
-t_y = bank["y"]
+def test_combinaison(combinaison):
+    """
+    Teste une combinaison sur un échantillon de 45000 valeurs prises dans bank-full.csv
 
-x_train, x_test, y_train, y_test = train_test_split(t_x, t_y, test_size=0.2, random_state=50)
+    :param combinaison: list, la combinaison testée
+    :return: affichage
+    """
+    global t_x
+    global modele
+    global colonnes_a_encoder
+    
+    bank = pandas.read_csv("bank-full.csv", sep=";")
+    bank["y"] = bank["y"].map({"no": 0, "yes": 1}).astype(int)
+    
+    colonnes_utiles = combinaison + ["y"]
+    bank = bank[colonnes_utiles]
 
-modele = KNeighborsClassifier(n_neighbors=5)
-modele.fit(x_train, y_train)
+    colonnes_a_encoder = [col for col in combinaison if col in non_numerical_descripteurs]
+    
+    if colonnes_a_encoder:
+        bank = pandas.get_dummies(bank, columns=colonnes_a_encoder, dtype=int)
 
-y_pred = modele.predict(x_test)
-
-score = metrics.accuracy_score(y_test, y_pred)
-print("Précision du modèle :", round(score*100, 2), "%")
+    t_x = bank.drop(columns=["y"])
+    t_y = bank["y"]
+    x_train, x_test, y_train, y_test = train_test_split(t_x, t_y, test_size=0.2, random_state=50)
+    
+    modele = KNeighborsClassifier(n_neighbors=5)
+    modele.fit(x_train, y_train)
+    
+    y_pred = modele.predict(x_test)
+    score = metrics.accuracy_score(y_test, y_pred)
+    print("Précision du modèle :", round(score*100, 2), "%")
 
 def predict_client(client_dict):
     """
@@ -32,7 +50,7 @@ def predict_client(client_dict):
     """
     client = pandas.DataFrame([client_dict])
     
-    client = pandas.get_dummies(client, columns=["job", "marital", "housing", "loan", "poutcome", "previous"], dtype=int)
+    client = pandas.get_dummies(client, columns=colonnes_a_encoder, dtype=int)
     
     #ajoute les colonnes manquantes pour que client_df ait les mêmes colonnes que les données d'entraînement
     for colonne in t_x.columns:
@@ -49,28 +67,45 @@ def predict_client(client_dict):
 
 #TESTS
 client_X = {
+    "age": 30,
     "job": "unemployed",
     "marital": "married",
+    "education": "primary",
+    "default": "no",
     "balance": 1787,
     "housing": "no",
     "loan": "no",
+    "contact": "cellular",
+    "day": 19,
+    "month": "oct",
     "duration": 79,
+    "campaign": 1,
     "pdays": -1,
     "previous": 0,
-    "poutcome": "unknown",
+    "poutcome": "unknown"
 }
 
 client_Y = {
-    "job": "management",
+    "age": 21,
+    "job": "student",
     "marital": "single",
-    "balance": 2536,
-    "housing": "yes",
+    "education": "secondary",
+    "default": "no",
+    "balance": 2488,
+    "housing": "no",
     "loan": "no",
-    "duration": 958,
-    "pdays": -1,
-    "previous": 0,
-    "poutcome": "unknown",
+    "contact": "cellular",
+    "day": 30,
+    "month": "jun",
+    "duration": 258,
+    "campaign": 6,
+    "pdays": 169,
+    "previous": 3,
+    "poutcome": "success"
 }
 
+combinaison = ['job', 'default', 'loan', 'campaign', 'poutcome']
+
+test_combinaison(combinaison)
 print("Prédiction pour le client X :", predict_client(client_X))
 print("Prédiction pour le client Y :", predict_client(client_Y))
